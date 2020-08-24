a437b54b1b41d020c5ca50abdb8e87839fc74e89
Ilya Kushlianski <kushliansky@gmail.com>
2020-08-24 19:54:28 +0300

    Minor refactorings

--- src/user/user.validation.ts
+++ src/user/user.validation.ts
@@ -2,21 +2,28 @@ import { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
 import { ErrorCodes } from '../errors/errors.enum';
 
+const LOGIN_RULES = Joi.string().max(30);
+
+// at least 1 letter and at least 1 digit
+const PASSWORD_REGEXP = new RegExp('^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$');
+
+const PASSWORD_RULES = Joi.string()
+  .pattern(PASSWORD_REGEXP)
+  .error(new Error('Password has incorrect format'));
+
+const AGE_RULES = Joi.number().min(4).max(130);
+const IS_DELETED_RULES = Joi.bool();
+
 export const validateCreateUser = (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
   const schema = Joi.object({
-    login: Joi.string().required().max(30),
-    password: Joi.string().required()
-      .pattern(
-        // at least 1 letter and at least 1 digit
-        new RegExp('^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$'),
-      )
-      .error(new Error('Password has incorrect format')),
-    age: Joi.number().required().min(4).max(130),
-    isDeleted: Joi.bool(),
+    login: LOGIN_RULES.required(),
+    password: PASSWORD_RULES.required(),
+    age: AGE_RULES.required(),
+    isDeleted: IS_DELETED_RULES,
   });
 
   const { error } = schema.validate(req.body);
@@ -38,15 +45,10 @@ export const validateUpdateUser = (
   }
 
   const schema = Joi.object({
-    login: Joi.string().max(30),
-    password: Joi.string()
-      .pattern(
-        // at least 1 letter and at least 1 digit
-        new RegExp('^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$'),
-      )
-      .error(new Error('Password has incorrect format')),
-    age: Joi.number().min(4).max(130),
-    isDeleted: Joi.bool(),
+    login: LOGIN_RULES,
+    password: PASSWORD_RULES,
+    age: AGE_RULES,
+    isDeleted: IS_DELETED_RULES,
   });
 
   const validation = schema.validate(req.body);
